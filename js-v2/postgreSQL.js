d1297df7c64e4e5ec0e2e0bcc70af0820210d98f
Mariana <mariananavarro3022@gmail.com>
2024-04-17 21:42:22 -0600

    comentarios

--- js-v2/postgreSQL.js
+++ js-v2/postgreSQL.js
@@ -1,62 +1,53 @@
-// Declare client as a global variable
-let client;
+let client;                                                 //Variable global
 
-function startClient() {
+function startClient() {                                    //Conexión con postreSQL
   const { Client } = require('pg');
 
-  // Create a new PostgreSQL client
   client = new Client({
-    user: 'master',
+    user: 'master',                                         //Todo esto es lo que tenemos en el docker (si se cambia hay que cambiarlo aqui tambien)
     password: 'pas1234',
-    host: 'localhost', // Change this to your PostgreSQL host
+    host: 'localhost', 
     database: 'postgresdb',
-    port: 5432, // Default PostgreSQL port
+    port: 5432, 
   });
 
-  // Connect to the database
-  client.connect();
+  client.connect();                                         //Conecta al cliente
 }
 
-function getUserData(targetUsername) {
-  // Ensure client is connected before executing the query
+//-------------------- QUERIES --------------------
+
+function getUserData(targetUsername) {                      //Permite obtener informacion del usuario                   
   if (!client) {
     console.error('Client is not connected.');
     return;
   }
 
   client.query(
-    'SELECT * FROM get_user_info_by_username($1)',
-    [targetUsername],
+    'SELECT * FROM get_user_info_by_username($1)', [targetUsername],
     (err, result) => {
       if (err) {
         console.error('Error executing query:', err);
       } else {
         const userInfo = result.rows[0];
         console.log('User Info:', userInfo);
-        // Process the retrieved data as needed
       }
-      // Close the database connection (optional, depending on your use case)
-      //client.end();
     }
   );
 }
 
-function closeConnection() {
-    // Close the database connection
-    client.end();
+function delay(ms) {                                        //le da tiempo a los queries de ser realizados
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
+
+
+async function closeConnection() {                          //Cierra la conexion con el cliente (unicamente cierrelo cuando el usuario ciera la aplicacion)
+    await delay(2000);
+    client.end();
+    }
+
+//-------------------- QUERIES --------------------
 
 startClient();
 getUserData('pame123');
 getUserData('mari456');
-
-function delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-async function closeConnection() {
-await delay(2000);
-client.end();
-}
-
 closeConnection();
