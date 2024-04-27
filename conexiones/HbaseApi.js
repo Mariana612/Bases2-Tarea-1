eb73ad09a48fea10025fd0a8eeb4141169f8dd26
Mariana <mariananavarro3022@gmail.com>
2024-04-26 22:24:47 -0600

    conexion hbase con api

---
+++ conexiones/HbaseApi.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const { putData, getData, getRowsContainingSubstring } = require('./Hbase.js');
+const app = express();
+
+app.use(cors());  // Enable CORS for all routes
+app.use(express.json());
+
+// Store Data
+app.post('/hbase/putData', async (req, res) => {
+    const { tableName, rowKey, columnFamily, columnQualifier, value } = req.body;
+    try {
+        await putData(tableName, rowKey, columnFamily, columnQualifier, value);
+        res.status(201).send('Data stored successfully');
+    } catch (err) {
+        console.error('Error storing data in HBase:', err);
+        res.status(500).send('Error storing data');
+    }
+});
+
+// Get Data by Row Key
+app.get('/hbase/getData/:tableName/:rowKey', async (req, res) => {
+    const { tableName, rowKey } = req.params;
+    try {
+        const result = await getData(tableName, rowKey);
+        if (result && result.length > 0) {
+            res.json(result);
+        } else {
+            res.status(404).send('No data found');
+        }
+    } catch (err) {
+        console.error('Error retrieving data from HBase:', err);
+        res.status(500).send('Error retrieving data');
+    }
+});
+
+// Search Data by Substring in Row Key
+app.get('/hbase/search/:tableName/:substring', async (req, res) => {
+    const { tableName, substring } = req.params;
+    try {
+        const result = await getRowsContainingSubstring(tableName, substring);
+        if (result && result.length > 0) {
+            res.json(result);
+        } else {
+            res.status(404).send('No data found');
+        }
+    } catch (err) {
+        console.error('Error searching data in HBase:', err);
+        res.status(500).send('Error searching data');
+    }
+});
+
+const port = 3004;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
